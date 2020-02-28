dfee65d6f389d0d9f51f87899c0d876eaf3a1ba9
Amy Eckert <amyeckert@Amys-MacBook-Pro.local>
2020-02-28 13:55:41 -0500

    completed gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,16 +1,11 @@
 // Given a moment, determine the moment that would be after a gigasecond has passed.
 // A gigasecond is 10^9 (1,000,000,000) seconds.
+// A millisecond is 1 ms = 0.001 s.
 
-// export 
-const gigasecond = () => {
-    let now = Date.now();
-    let moment = new Date();
-    moment.setHours(0,0,0,0); // midnight
-    let midnight = moment;
-    // console.log(now);
+export const gigasecond = (moment) => {
+    let addInterval = (moment.getTime() + (1000000000 / 0.001));
+    const momentLater = new Date(addInterval);
 
-    console.log(midnight);
-
+    return momentLater;
 };
 
-gigasecond();
