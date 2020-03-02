93d1ca3fed29178df56539582c5f7e90fdc464ab
Amy Eckert <amyeckert@Amys-MacBook-Pro.local>
2020-03-02 09:30:00 -0500

    finished armstrong numbers exercise

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -1,8 +1,23 @@
-//
-// This is only a SKELETON file for the 'Armstrong numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
 
-export const isArmstrongNumber = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isArmstrongNumber = (number) => {
+    // convert input number to a string array, then convert each string item back to a number
+    let numbers =  Array.from(String(number), Number);
+    let total= [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        // raise each number in numbers to the power of the total count of numbers
+        total.push(Math.pow(numbers[i], numbers.length));
+    }
+    // Get sum of numbers
+    var sum = total.reduce(function(a, b){
+        return a + b;
+    }, 0);
+
+    if (sum === number ) {
+        return true;
+    }
+
+    return false;
+
 };
